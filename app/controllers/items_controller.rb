7ef2104c5f0c4afc419e5db34dcf86f7e4c19ead
hikaru0203 <h2.soccer.3h@i.softbank.jp>
2021-04-11 23:50:31 +0900

    商品の編集、更新

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,20 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update 
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
+
 
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :condition_id, :cost_id, :prefecture_id, :ship_date_id, :price, :image).merge(user_id: current_user.id)

1645137694699af4fb13042df2c0058f8de92ea6
hikaru0203 <h2.soccer.3h@i.softbank.jp>
2021-04-12 16:44:23 +0900

    訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   
   
   def index

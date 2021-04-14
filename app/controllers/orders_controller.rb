cf9fece1dd80bca4174b326f8125b585cf518725
hikaru0203 <h2.soccer.3h@i.softbank.jp>
2021-04-14 20:21:02 +0900

    修正７

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
-  before_action :move_to_index, only: [:index, :create]
   before_action :set_item, only:[:index, :create]
+  before_action :move_to_index, only: [:index, :create]
+  
   def index
     @pay_form = PayForm.new
   end
@@ -9,12 +10,7 @@ class OrdersController < ApplicationController
   def create
     @pay_form = PayForm.new(order_params)
     if @pay_form.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 自身のPAY.JPテスト秘密鍵を記述しましょう
-      Payjp::Charge.create(
-        amount: @item.price,  # 商品の値段
-        card: order_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
+      pay_item
       @pay_form.save
       redirect_to root_path
     else
@@ -37,6 +33,15 @@ class OrdersController < ApplicationController
   @item = Item.find(params[:item_id])
  end
 
+ def pay_item
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price,  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+    end
+    
 
 
 end

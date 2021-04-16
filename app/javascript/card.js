0088ce25fd5572c446f0001c895dcaec3995da3c
hikaru0203 <h2.soccer.3h@i.softbank.jp>
2021-04-16 12:48:39 +0900

    console.log2

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,5 @@
-console.log(test)
 const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  Payjp.setPublicKey("pk_test_ad62af1a158fbe49c35b8b33");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
